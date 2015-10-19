036b531dfea09a0e2746402d95814edacadcf919
etudiant <etudiant@LD140-M42-02802.local>
2015-10-19 18:12:52 -0400

    Version finale

---
+++ projet2/Affiche.swift
@@ -0,0 +1,14 @@
+//
+//  affiche.swift
+//  projet2
+//
+//  Created by etudiant on 2015-10-19.
+//  Copyright (c) 2015 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class Affiche: UICollectionViewCell {
+    @IBOutlet weak var Nom: UILabel!
+    @IBOutlet weak var Image: UIImageView!
+}
