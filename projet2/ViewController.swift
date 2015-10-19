689264b18389b82dfaab87c54cb9cdb316232167
etudiant <etudiant@LD140-M42-02802.local>
2015-10-19 15:49:15 -0400

    Initial Commit

---
+++ projet2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projet2
+//
+//  Created by etudiant on 2015-10-19.
+//  Copyright (c) 2015 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
